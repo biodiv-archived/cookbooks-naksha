01f12895c49a92c2f97d4a8fd3a93d99db0e33ae
Sravanthi <sravanthi@strandls.com>
2018-07-15 14:58:12 +0530

    Fixed naksha war path

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default[:naksha][:directory] = "/usr/local/src"
 
 default[:naksha][:link]      = "https://codeload.github.com/strandls/#{naksha.repository}/zip/#{naksha.version}"
 default[:naksha][:extracted] = "#{naksha.directory}/#{naksha.appname}-#{naksha.version}"
-default[:naksha][:war]       = "#{naksha.extracted}/target/naksha.war"
+default[:naksha][:war]       = "#{naksha.extracted}/build/libs/naksha.war"
 default[:naksha][:download]  = "#{naksha.directory}/#{naksha.repository}-#{naksha.version}.zip"
 
 default[:naksha][:home] = "/usr/local/naksha"
